363952590fe36aae82b8ab6fea275f2d5e8dffea
JayaswalPrateek <jayaswalprateek@gmail.com>
2023-10-26 15:07:18 +0530

    Restructured Files: Removed backend_src

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Checking updates for dependencies" &&
+    go get -u && go mod tidy &&
+    echo -e "DONE\n"
+
+echo "Linting" &&
+    go fmt ./main.go && go vet ./main.go && golangci-lint run ./main.go &&
+    echo -e "DONE\n"
+
+echo "Generating executable" &&
+    go build -ldflags="-s -w" -o backendBinary_LINUX main.go &&
+    GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o backendBinary_WINDOWS.exe main.go &&
+    echo -e "DONE"
